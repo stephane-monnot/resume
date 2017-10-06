6dd485708a9cba9c312220c0a301899ff45c2c25
Stéphane Monnot <monnot.stephane@gmail.com>
2017-10-06 18:47:55 +0200

    Update Remaining days until japan styles

--- src/WaitingUntilJapanScreen.js
+++ src/WaitingUntilJapanScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import flow from 'lodash/flow';
 import { LinearProgress } from 'material-ui/Progress';
-import Card, { CardContent } from 'material-ui/Card';
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import { withTheme } from 'material-ui/styles';
+
+import './WaitingUntilJapanScreen.css';
 
 class WaitingUntilJapanScreen extends Component {
 
@@ -9,30 +13,62 @@ class WaitingUntilJapanScreen extends Component {
     const untilDate = new Date(2018, 0, 15);
     const today = new Date();
     const timeDiff = Math.abs(untilDate.getTime() - today.getTime());
-    const remainingDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    const remainingDays = Math.round(timeDiff / (1000 * 3600 * 24));
     const timeDiff2 = Math.abs(today.getTime() - fromDate.getTime());
-    const spentDays = Math.ceil(timeDiff2 / (1000 * 3600 * 24));
+    const spentDays = Math.round(timeDiff2 / (1000 * 3600 * 24));
     const percent = Math.round(spentDays / (remainingDays + spentDays) * 100 * 10) / 10;
 
+    const { theme } = this.props;
+    const primaryColor = theme.palette.primary[500];
+    const secondaryColor = theme.palette.secondary[500];
+
+    const styles = {
+      primaryColor: {
+        background: primaryColor,
+        color: '#fff',
+      },
+      secondaryColor: {
+        background: secondaryColor,
+        color: '#fff',
+      },
+    };
+
     return (
-      <Card style={{maxWidth: '90%', margin: '0 auto'}}>
+      <Card className="WaitingUntilJapanScreen-card">
         <CardContent>
           <h3>Remaining days until japan</h3>
 
           <div>
-            From : { fromDate.toLocaleDateString() }<br />
-            Until : { untilDate.toLocaleDateString() }<br />
-            Spent days : { spentDays } days<br />
-            Remaining days : { remainingDays } days<br />
-            Percent : { percent }%<br />
-            <LinearProgress mode="determinate" value={percent}/>
+            <LinearProgress color="accent" mode="determinate" value={percent}/>
           </div>
 
         </CardContent>
+        <CardActions className="WaitingUntilJapanScreen-card-footer" style={styles.primaryColor}>
+          <div className="WaitingUntilJapanScreen-info">
+            <div className="WaitingUntilJapanScreen-info-title">From</div>
+            <div className="WaitingUntilJapanScreen-info-value">{ fromDate.toLocaleDateString() }</div>
+          </div>
+          <div className="WaitingUntilJapanScreen-info">
+            <div className="WaitingUntilJapanScreen-info-title">Until</div>
+            <div className="WaitingUntilJapanScreen-info-value">{ untilDate.toLocaleDateString() }</div>
+          </div>
+          <div className="WaitingUntilJapanScreen-info">
+            <div className="WaitingUntilJapanScreen-info-title">Days</div>
+            <div className="WaitingUntilJapanScreen-info-value">{ spentDays } / { remainingDays }</div>
+          </div>
+          <div className="WaitingUntilJapanScreen-info">
+            <div className="WaitingUntilJapanScreen-info-title">Percent</div>
+            <div className="WaitingUntilJapanScreen-info-value">{ percent }%</div>
+          </div>
+        </CardActions>
       </Card>
 
     );
   }
 }
 
-export default WaitingUntilJapanScreen;
+const decorators = flow([
+  withTheme,
+]);
+
+export default decorators(WaitingUntilJapanScreen);
