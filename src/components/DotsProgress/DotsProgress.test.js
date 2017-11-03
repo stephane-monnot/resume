5e524d609a0b2150d6d5d4b4488828e66d89a3cf
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-03 21:21:49 +0100

    Add DotsProgress test file

---
+++ src/components/DotsProgress/DotsProgress.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DotsProgress from './DotsProgress';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DotsProgress numberOfActiveDots={5} maxNumberOfDots={10} />, div);
+});
