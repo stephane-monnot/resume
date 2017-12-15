127b91d48737aebe4aea3de3b3bf80dad2b28a2d
Monnot Stéphane <monnot.stephane@gmail.com>
2017-12-15 01:34:15 +0100

    Add new custom icon for Apache Cordova

---
+++ src/components/Icons/Apachecordova/Apachecordova.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SVGDeviconInline from 'react-devicon/_base/SVGDeviconInline';
+
+import iconSVG from './Apachecordova.svg'
+
+/** Apachecordova */
+function Apachecordova({ width, height, className }) {
+  return (
+    <SVGDeviconInline
+      className={`Apachecordova ${className}`}
+      iconSVG={iconSVG}
+      width={width}
+      height={height}
+    />
+  )
+}
+
+Apachecordova.propTypes = {
+  className: PropTypes.string,
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+}
+
+export default Apachecordova
