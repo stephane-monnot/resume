a0f3b81d939b270c51b65664ac3b63cc5a4d215c
Monnot Stéphane <monnot.stephane@gmail.com>
2018-01-05 13:56:41 +0100

    Load fonts with webfont

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+import WebFont from 'webfontloader';
 import App from './App';
 import './index.css';
 import configureStore from './store/configureStore';
 
 const store = configureStore({});
+
+WebFont.load({
+  google: {
+    families: [
+      'Material Icons',
+      'Roboto:400,500',
+      'Open Sans:400,600,700,800,300'
+    ]
+  }
+});
 
 render(
   <Provider store={store}>
