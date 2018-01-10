ae9fc8275d9054b8f49a67180845223b9f3c9be4
Monnot Stéphane <monnot.stephane@gmail.com>
2018-01-10 21:23:34 +0100

    Fix unused error

--- src/IntlRoute.js
+++ src/IntlRoute.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { connect } from "react-redux";
-import { changeLanguage } from "./actions";
 
 const IntlRoute = ({ language, component: Component, ...rest, onChangeLanguage }) => (
   <Route
