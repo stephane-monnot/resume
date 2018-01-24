dfb627a56963c225285cc98068861039e083fc6a
Monnot Stéphane <monnot.stephane@gmail.com>
2018-01-24 17:28:31 +0100

    Add 404 page and upgrade react to v16

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -3,15 +3,47 @@ import flow from 'lodash/flow';
 import Helmet from 'react-helmet';
 import { injectIntl } from 'react-intl';
 import ScreenBlock from '../../components/ScreenBlock/ScreenBlock';
-import GridBackground from '../../components/GridBackground/GridBackground';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { monokai } from 'react-syntax-highlighter/styles/hljs';
+import { FakeCodeTyping } from 'react-fake-code-typing';
+
+import 'react-fake-code-typing/style.min.css';
 
 import './NotFound.css';
-import appTheme from "../../theme";
+
+const testCode = `
+/**
+ * @link Home
+ * @todo ..write a 404 page that actually makes sense.
+ */
+class HttpException extends \\RuntimeException implements HttpExceptionInterface
+{
+  public function __construct(int $statusCode, string $message = null, \\Exception $previous = null)
+  {
+      $this->statusCode = $statusCode;
+      parent::__construct($message, $code, $previous);
+  }
+
+  /**
+   * Get Status code.
+   *
+   * @return int Status code
+   */
+  public function getStatusCode()
+  {
+      return $this->statusCode;
+  }
+}
+
+/**
+ * @link Home
+ * @todo ..write a 404 page that actually makes sense.
+ */
+throw new HttpException(400, 'Page Not Found');`;
 
 class NotFound extends Component {
   render() {
     const { formatMessage } = this.props.intl;
-    const binaryCode404 = '0000000110010100';
 
     return (
       <div className="NotFound">
@@ -22,9 +54,9 @@ class NotFound extends Component {
           })}
         />
         <ScreenBlock className="NotFoundBlock">
-          <div className="container">
-            404 - Page Not Found
-          </div>
+          <FakeCodeTyping>
+            <SyntaxHighlighter language="php" style={monokai}>{testCode}</SyntaxHighlighter>
+          </FakeCodeTyping>
         </ScreenBlock>
       </div>
     );
